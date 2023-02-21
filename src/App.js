7574f43af06c4524352a737ad8102b3b1bcfae3c
beautytasara27 <beautytasara@gmail.com>
2023-02-21 19:31:15 +0200

    animation on loader

--- src/App.js
+++ src/App.js
@@ -3,8 +3,28 @@ import Home from "./pages/home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Data } from "./data/presets";
 import AppContext from "./context/AppContext";
+import { useState, useEffect } from "react";
 function App() {
+  const [isLoading, setLoading] = useState(true);
   
+  function someRequest() { 
+    return new Promise(resolve => setTimeout(() => resolve(), 2500));
+  } 
+
+  useEffect(() => {
+    someRequest().then(() => {
+      const loaderElement = document.querySelector(".loader-container");
+      if (loaderElement) {
+        loaderElement.remove();
+        setLoading(!isLoading);
+      }
+    });
+  });
+
+  if (isLoading) { 
+    return null;
+  }
+
   return (
     <AppContext.Provider value={Data}>
       <div className="App">
